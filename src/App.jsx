4e945d633159baee8f9512ef56a1eee3db943fa5
nicoargiolas <argiolas.nicola00@gmail.com>
2025-06-18 15:43:13 +0200

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { useState } from 'react';
 import './App.css'
 
-// 📌 Milestone 1: Creare un Form con Campi Controllati
+// 📌 Milestone 2: Validare in tempo reale
 
-// Crea un form di registrazione con i seguenti campi controllati (gestiti con useState):
-// ✅ Nome completo (input di testo)
-// ✅ Username (input di testo)
-// ✅ Password (input di tipo password)
-// ✅ Specializzazione (select con opzioni: "Full Stack", "Frontend", "Backend")
-// ✅ Anni di esperienza (input di tipo number)
-// ✅ Breve descrizione sullo sviluppatore (textarea)
-// Aggiungi una validazione al submit, verificando che:
-// Tutti i campi siano compilati
-// L'input Anni di esperienza sia un numero positivo
-// La Specializzazione sia selezionata
+// Aggiungere la validazione in tempo reale dei seguenti campi:
+// ✅ Username: Deve contenere solo caratteri alfanumerici e almeno 6 caratteri (no spazi o simboli).
+// ✅ Password: Deve contenere almeno 8 caratteri, 1 lettera, 1 numero e 1 simbolo.
+// ✅ Descrizione: Deve contenere tra 100 e 1000 caratteri (senza spazi iniziali e finali).
+// Suggerimento: Per semplificare la validazione, puoi definire tre stringhe con i caratteri validi e usare .includes() per controllare se i caratteri appartengono a una di queste categorie:
 
-// Al submit, se il form è valido, stampa in console i dati.
+// const letters = "abcdefghijklmnopqrstuvwxyz";
+// const numbers = "0123456789";
+// const symbols = "!@#$%^&*()-_=+[]{}|;:'\\",.<>?/`~";
+// Per ciascuno dei campi validati in tempo reale, mostrare un messaggio di errore (rosso) nel caso non siano validi, oppure un messaggio di conferma (verde) nel caso siano validi.
 
 function App() {
   const [name, setName] = useState('');
@@ -43,9 +40,40 @@ function App() {
     }
   }
 
+  const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/`~";
+
+  const usernameValidation = () => {
+
+    // Regex: ^[a-zA-Z0-9]+$
+    // Le regex in JavaScript si scrivono racchiuse tra slash: /.../
+    // ^     --> Inizio stringa
+    // $     --> Fine stringa
+    // [a-zA-Z0-9] --> Caratteri consentiti: lettere maiuscole, minuscole e numeri
+    // +     --> Dev'esserci almeno uno di questi caratteri (uno o più)
+    // .test(stringa) verifica che la stringa rispetti la regex e restituisce true/false
+
+    return /^[a-zA-Z0-9]+$/.test(username) && username.length >= 6
+  }
+
+  const passwordValidation = () => {
+    const hasLetter = [...password].some(c => letters.includes(c));
+    const hasNumber = [...password].some(c => numbers.includes(c));
+    const hasSymbol = [...password].some(c => symbols.includes(c));
+
+    return password.length >= 8 && hasLetter && hasNumber && hasSymbol
+  }
+
+  const descriptionValidation = () => {
+    return description.trim().length >= 100 && description.trim().length <= 1000
+  }
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        autoComplete="off">
         <section>
           <input
             type='text'
@@ -62,6 +90,10 @@ function App() {
             value={username}
             onChange={e => setUsername(e.target.value)}
           />
+          <p style={{ color: usernameValidation() ? 'green' : 'red' }}>
+            {usernameValidation() ? "L'username è valido" : "Devi inserire solo caratteri alfanumerici e almeno 6 caratteri"}
+          </p>
+
         </section>
 
         <section>
@@ -71,6 +103,9 @@ function App() {
             value={password}
             onChange={e => setPassword(e.target.value)}
           />
+          <p style={{ color: passwordValidation() ? 'green' : 'red' }}>
+            {passwordValidation() ? "La password è valida" : "Devi inserire almeno 8 caratteri, 1 lettera, 1 numero e 1 simbolo"}
+          </p>
         </section>
 
         <section>
@@ -100,6 +135,9 @@ function App() {
             value={description}
             onChange={e => setDescription(e.target.value)}
           />
+          <p style={{ color: descriptionValidation() ? 'green' : 'red' }}>
+            {descriptionValidation() ? "La descrizione è valida" : "Devi inserire tra 100 e 1000 caratteri"}
+          </p>
         </section>
 
         <button> Invia </button>
